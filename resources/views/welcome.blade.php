fc0bef2778efabe2e030453a8437ae32431dc1b6
Miotava <naru3register@gmail.com>
2019-04-16 16:31:34 +0900

    h2アウトラインをトップページに追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,29 @@
 @section('title','Welcome! | Miotava\'s Portfolio')
 
 @section('content')
-<h1>Miotavaの個人ポートフォリオへようこそ！</h1>
+<div class="main-title">
+    <h1>Welcome to Miotava's Portforio Site!!</h1>
+</div>
 
+<h2>経歴</h2>
+<div class="lg-section">
+    ...
+</div>
+
+<h2>スキル</h2>
+<div class="lg-section">
+    ...
+</div>
+
+<h2>作品</h2>
+<div class="lg-section">
+    ...
+</div>
+
+<h2>関連</h2>
+<div class="lg-section">
+    ...
+</div>
 
 @endsection
 
