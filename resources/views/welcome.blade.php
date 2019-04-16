a40688a25203ecaca489db9a5653002c75cd44ed
Miotava <naru3register@gmail.com>
2019-04-17 07:16:56 +0900

    BuppanFlowの紹介ページに詳細情報を追加しスタイルを整形 - 概要、機能、技術、関連リンクの項目をViewに追加 - 画像拡大jQueryプラグインを追加 - トップページにもBuppanFlow紹介ページへのリンクを追加 - LaravelデフォルトのWelcomeページを削除

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,16 @@
 
 <h2>作品</h2>
 <div class="lg-section">
-    ...
+    <h3>物販情報管理アプリ「BuppanFlow」</h3>
+    <div class="md-section">
+        BuppanFlowは主に輸入物販を行う個人〜中小事業者向けの管理・情報アプリです。
+        詳細は以下リンクよりご覧ください。<br>
+        <a href={{ route('aboutBuppanFlow') }} target="_blank">BuppanFlowの詳細</a>
+    </div>
 </div>
 
 <h2>関連</h2>
 <div class="lg-section">
-    ...
 </div>
 
 @endsection
