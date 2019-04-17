ad741a4a2b82411f4b856181fb7d1ab09664f968
Miotava <naru3register@gmail.com>
2019-04-17 09:59:57 +0900

    added google analytics code to default template file

--- resources/views/layouts/defaultLayout.blade.php
+++ resources/views/layouts/defaultLayout.blade.php
@@ -2,6 +2,20 @@
 <html lang="ja">
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123562526-4"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-123562526-4');
+
+    </script>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title')</title>
